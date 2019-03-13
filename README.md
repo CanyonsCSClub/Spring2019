1a875548f9edf3bd4229aa818e52c9b232a3ac05
Persomatey <huntergoodin1@hotmail.com>
2019-03-12 21:14:33 -0700

    Initial Push To Master
    Included the following: 
    - Added .git meta 
    - Added .gitignore 
    - Added README.txt 
    - Added the basic Unity project 
    - The movement system: Player object can move using WASD and jump using Spacebar 
    - Made a player prefab out of the player Object in the prefabs folder. Feel free to use that in any of your scenes regarding the player and feel free to edit the player prefab if you need to for your project.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Spring2019
+Hello and welcome to the Computer Science Club's Spring 2019 project! 
+
+Each of you has been assigned a task that you can view here: https://docs.google.com/spreadsheets/d/1t-LP74rc4Cb3FGGQ_dNucH8P1wZABYSJn35-6Eegefs/edit?usp=sharing
+
+Please be running on this version of Unity: https://download.unity3d.com/download_unity/eb4bc6fa7f1d/UnityDownloadAssistant-2017.4.22f1.exe 
+
+When tackling your project, you will be expected to make a new branch and do your work on that branch. When you are done with your project, you can feel free to make a pull request. When the request is seen, your work will be evaluated and then added to the game. 
+
+Thank you, and good luck! I know you will all do great! 
+
+- Hunter Goodin 
+
---
+++ Spring2019/Assets/Scripts/Player/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Programmer:   Hunter Goodin 
+ * Date Created: 03/12/2019 @  8:15 PM 
+ * Last Updated: 03/12/2019 @  8:30 PM by Hunter Goodin
+ * File Name:    CoreMovement.cs 
+ * Description:  This script will be responsible for the camera's movement. 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;   // To be populated with the player in-engine 
+    private Vector3 offset;     // The offset the camera will be from the player 
+
+    void Start()
+    {
+        // offset is = to wherever the camera is on initialization subtracted by where the player is on initialization 
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        // move the camera to wherever the player is plus the offset 
+        transform.position = player.transform.position + offset;
+    }
+}
