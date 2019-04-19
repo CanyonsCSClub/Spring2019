3af0308fc04f366748f43fe3e5199666058ec4bd
Dylan <masterdylan87@yahoo.com>
2019-04-19 12:56:38 -0700

    Update 2.0 NOW WITH SCRIPTS! (not done)

---
+++ Spring2019/Assets/Scripts/Pause/Quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour {
+
+	public void DylanIsDumb(){
+		Debug.Log ("It works!");
+		Application.LoadLevel(0); 
+
+	}
+}
