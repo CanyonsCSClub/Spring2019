f5b4252fc056ccab8214c482883d60ed31e2c31c
Madian Jaber <madianjaber@gmail.com>
2019-04-26 11:00:13 -0700

    Minima working

---
+++ Spring2019/Assets/Scripts/Minimap/MmCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MmCamera : MonoBehaviour {
+
+    public Vector3 offset;
+    public GameObject player;
+    public Transform Minicamera;
+
+    // Update is called once per frame
+    void Update () {
+        //change position of mini map camera to be above player
+        Minicamera.position = player.transform.position + offset;
+    }
+}
