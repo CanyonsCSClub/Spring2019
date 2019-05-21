8993ff4393ab9e6c1f826745c8e536255a0efd75
Dylan <masterdylan87@yahoo.com>
2019-05-21 12:05:36 -0700

    Update 2.5

--- Spring2019/Assets/Scripts/Haz/Indi.cs
+++ Spring2019/Assets/Scripts/Haz/Indi.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 /*01001101 01100001 01100011 00100111 01110011 00100000 01100011 01100001 01101110 
  *00100000 01100111 01101111 00100000 01110100 01101111 00100000 01101000 01100101 01101100 01101100 */
 public class Indi : MonoBehaviour {
-	public float speed = 1f;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+	public GameObject Indiana;
+	private float speed = 0.5f;
+	private int time = 0;
+
 	void Update () {
-		transform.Rotate (0, 0, speed); //makes it roll
+		if (Indiana == true) {
+			if (time <= 60) {
+				transform.Translate (speed, 0, 0);// moveing forward
+			}
+			if (time >= 60) {
+				Indiana.SetActive (false);
+			}
+		}
 	}
 }
---
+++ Spring2019/Assets/Scripts/Haz/Indi2.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//this is so it can spin and move at once
+public class Indi2 : MonoBehaviour {
+	private float speed = 5f;
+	void Update () {
+		transform.Rotate (0, 0, speed * -1);
+	}
+}
