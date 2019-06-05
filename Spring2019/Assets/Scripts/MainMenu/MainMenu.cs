683e479872401d1259ae69d382519aac3b336f8e
Madian Jaber <madianjaber@gmail.com>
2019-06-04 22:01:56 -0700

    Main menu

---
+++ Spring2019/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class MainMenu : MonoBehaviour {
+
+
+    
+
+    public void playButton1(string scenename)
+    {
+        Application.LoadLevel(scenename);
+        
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
